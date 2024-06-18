a978918f972b609d7903fe00b13007eca47ce1f9
Andrey Sery <andrey.72006@gmail.com>
2024-06-18 15:02:27 +0300

    getting data from form (output to the consol)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ window.addEventListener("DOMContentLoaded", () => {
   const buttonCallForlModalWindow = document.querySelector(".btn__try")
   const modalWindow = document.querySelector(".modal")
   const buttonCloseModalWindow = document.querySelectorAll(".btn__close")
+  const formElement = document.querySelector(".form")
 
   buttonCallForlModalWindow.addEventListener("click", (e) => {
     e.preventDefault()
@@ -24,5 +25,16 @@ window.addEventListener("DOMContentLoaded", () => {
       }
 
     })
+  })
+
+  formElement.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const formData = new FormData(formElement)
+    const data = {}
+    formData.forEach((value, key) => {
+      data[key] = value
+    })
+    console.log(data)
   })
 })
